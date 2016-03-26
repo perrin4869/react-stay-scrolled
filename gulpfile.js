f565dcbe7b6ba894da6cb54befabda9727bb82dc
Julian Grinblat <julian@dotcore.co.il>
2016-03-27 01:11:32 +0900

    Gulpfile passes lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const gulp = require('gulp'),
-	babel  = require('gulp-babel'),
-	eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
-gulp.task('lint', function() {
-	return gulp.src(['lib/index.jsx'])
-	.pipe(eslint())
-	.pipe(eslint.format());
-})
+gulp.task('lint', () => (
+  gulp.src(['lib/index.jsx'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+));
 
-gulp.task('build', ['lint'], function() {
-	gulp.src('lib/index.jsx')
-	.pipe(babel())
-	.pipe(gulp.dest('dist'));
-});
+gulp.task('build', ['lint'], () => (
+  gulp.src('lib/index.jsx')
+  .pipe(babel())
+  .pipe(gulp.dest('dist'))
+));
 
 gulp.task('default', ['build']);
