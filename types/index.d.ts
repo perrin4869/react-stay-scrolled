ed59bd441c05fcd8a4c2a60fca4a0cbd8b473e9f
Julian Grinblat <julian@dotcore.co.il>
2020-12-24 14:16:25 +0900

    Add typescript typings

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export interface UseStayScrolled {
+    scroll: (position: number) => void;
+    stayScrolled: () => void;
+    scrollBottom: () => void;
+    isScrolled: () => boolean;
+}
+
+export interface UseStayScrolledOptions {
+    initialScroll: number | null;
+    inaccuracy: number;
+    runScroll?: (offset: number) => void;
+}
+
+export default function useStayScrolled(
+    domRef: React.RefObject<HTMLElement>,
+    options: UseStayScrolledOptions,
+): UseStayScrolled;
